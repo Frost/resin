488d52aac28e34332cf24cdfb11b33ab3ef995eb
Martin Frost <frost@ceri.se>
2019-03-14 22:47:50 +0100

    Update credo

--- mix.exs
+++ mix.exs
@@ -2,12 +2,14 @@ defmodule Resin.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :resin,
+    [
+      app: :resin,
       version: "0.4.1",
       elixir: "~> 1.0",
-      description: description,
-      package: package,
-      deps: deps]
+      description: description(),
+      package: package(),
+      deps: deps()
+    ]
   end
 
   def application do
@@ -21,18 +23,20 @@ defmodule Resin.Mixfile do
   end
 
   defp package do
-    [ files: ~w[ lib README.md mix.exs LICENSE ],
+    [
+      files: ~w[ lib README.md mix.exs LICENSE ],
       contributors: ["Martin Frost"],
       licenses: ["The MIT License (MIT)"],
-      links: %{
-        "GitHub" => "https://github.com/Frost/resin",
-        "Docs" => "http://hexdocs.pm/resin/" } ]
+      links: %{"GitHub" => "https://github.com/Frost/resin", "Docs" => "http://hexdocs.pm/resin/"}
+    ]
   end
 
   defp deps do
-    [{:plug, "~> 1.0"},
-     {:earmark, ">= 0.0.0", only: :dev},
-     {:credo, "~> 0.1", only: :dev},
-     {:ex_doc, "~> 0.10", only: :dev}]
+    [
+      {:plug, "~> 1.0"},
+      {:earmark, ">= 0.0.0", only: :dev},
+      {:credo, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.10", only: :dev}
+    ]
   end
 end
