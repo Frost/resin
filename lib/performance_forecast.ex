ebfea11c48d89d0952471e7e00dfcee40a3f6edb
Martin Frost <frost@ceri.se>
2019-03-14 22:56:55 +0100

    Add moduledoc to PerformanceForecast

--- lib/performance_forecast.ex
+++ lib/performance_forecast.ex
@@ -1,4 +1,11 @@
 defmodule PerformanceForecast do
+  @moduledoc """
+  An agent that keeps track of your enterpriseyness patterns.
+
+  If you configure Resin to have a varying pattern of enterpriseyness,
+  it will use this module to cycle through your pattern.
+  """
+
   def init(enterpriseyness) when is_list(enterpriseyness) do
     Agent.start_link(fn -> enterpriseyness end)
   end
